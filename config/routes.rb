de583cbeb0851eec1d29cae48d8d8124d5f09b53
北添真広 <sas.surfer0jonny@gmail.com>
2018-07-22 15:03:20 +0900

    configure routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :groups, except: [:show, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
   root 'messages#index'
 end
