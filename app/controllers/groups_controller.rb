2f06e1479f70574ac83bb173ec8ae2074437f993
北添真広 <sas.surfer0jonny@gmail.com>
2018-07-23 15:36:18 +0900

    add groups#edit&update and then set group#index as root_path

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,18 +15,32 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      flash[:notice] = "グループを作成しました"
-      redirect_to :root
+      save_success("グループを作成しました")
     else
       render action: :new
     end
   end
 
-    private
-    def group_params
-      params.require(:group).permit(
-        :name,
-        { user_ids: [] }
-        )
+  def update
+    @group = Group.find(params[:id])
+    binding.pry
+    if @group.update_attributes(group_params)
+      save_success("グループ情報を更新しました")
+    else
+      render action: :edit
     end
+  end
+
+    private
+      def group_params
+        params.require(:group).permit(
+          :name,
+          { user_ids: [] }
+          )
+      end
+
+      def save_success(message)
+        flash[:notice] = message
+        redirect_to :root
+      end
 end
