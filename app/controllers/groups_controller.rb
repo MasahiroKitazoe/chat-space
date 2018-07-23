60f520b8f676304291f5eb6e171775af5d6f6900
北添真広 <sas.surfer0jonny@gmail.com>
2018-07-23 13:01:47 +0900

    add groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,4 +8,17 @@ class GroupsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
   end
+
+  def create
+    Group.create(group_params)
+    redirect_to :root
+  end
+
+    private
+    def group_params
+      params.require(:group).permit(
+        :name,
+        { user_ids: [] }
+        )
+    end
 end
