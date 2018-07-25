9132d4f43f950ea8eadc532a06a5868b5829989b
北添真広 <sas.surfer0jonny@gmail.com>
2018-07-25 12:35:41 +0900

    add js file for asynchronous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,36 @@
+$(function(){
+  function buildHTML(message){
+    var html = `<li class='chat-message'>
+                  <div class='chat-message__header'>
+                    <p class='chat-message__name'>${message.user.name}</p>
+                    <p class='chat-message__time'>${message.created_at}</p>
+                  </div>
+                    <p class='chat-message__body'>${message.body}</p>
+                </li>
+                `
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var group_id = 
+    $.ajax({
+      type: "POST",
+      url: "/groups/${group_id}/messages",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-messages').append(html)
+      $('#message_body').val('')
+      $('#message_image').val('')
+    })
+    .fail(function(){
+    alert('error');
+    })
+  })
+});
